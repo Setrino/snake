24e546c84f1aacce7cba375d45c0c9139062756a
Setrino <sk@fkdn-lab.com>
2013-04-03 19:22:23 +0200

    Add user online tab

---
+++ templates/no_online.php
@@ -0,0 +1,38 @@
+<?php
+
+session_name('tzLogin');
+session_set_cookie_params(2*7*24*60*60);
+session_start();
+
+define('INCLUDE_CHECK',true);
+
+require_once "login.php";
+require "functions.php";
+
+// We don't want web bots scewing our stats:
+if(is_bot()) die();
+
+$stringIp = $_SERVER['REMOTE_ADDR'];
+
+if($_SESSION['id']){
+// Checking wheter the visitor is already marked as being online:
+    $status = mysql_query("SELECT status FROM online WHERE id='".$_SESSION['id']."'");
+
+    if(mysql_fetch_row($status)[0] == 0){
+
+        mysql_query("UPDATE online SET status=1 WHERE id='".$_SESSION['id']."'");
+        mysql_query("UPDATE online SET last_on=NOW() WHERE id='".$_SESSION['id']."'");
+
+        }
+}
+
+// Removing entries not updated in the last 10 minutes:
+//mysql_query("UPDATE online SET status=0 WHERE last_on < SUBTIME(NOW(), '0 0:10:0')");
+//mysql_query("DELETE FROM tz_who_is_online WHERE dt<SUBTIME(NOW(),'0 0:10:0')");
+
+// Counting all the online visitors:
+list($totalOnline) = mysql_fetch_array(mysql_query("SELECT COUNT(*) FROM online WHERE status=1"));
+
+// Outputting the number as plain text:
+echo $totalOnline;
+?>
---
+++ templates/users_online.php
@@ -0,0 +1,50 @@
+<script type="text/javascript">
+    function statusIcon(status, callback){
+
+        switch(status){
+            case(0):
+                callback("../images/offline.png");
+                break;
+            case(1):
+                callback("../images/online.png");
+                break;
+            case(2):
+                callback("../images/away.png");
+                break;
+        }
+    }
+</script>
+
+<?php
+
+define('INCLUDE_CHECK',true);
+
+require_once "login.php";
+require "functions.php";
+
+// We don't want web bots accessing this page:
+if(is_bot()) die();
+
+// Selecting the top 15 countries with the most visitors:
+$counter = 0;
+$result = mysql_query("	SELECT nick,status
+						FROM online
+						ORDER BY status DESC
+						LIMIT 15");
+while($row=mysql_fetch_assoc($result))
+{
+	echo '
+	<div class="geoRow">
+		<div class="avatar"><img src="'.mysql_fetch_row(mysql_query("SELECT avatar FROM users WHERE nick='".$row['nick']."'"))[0].'"
+		 width="11" height="11" /></div>
+		<div class="user">'.$row['nick'].'</div>
+		<div class="status"><img id="status_icon'.$counter.'" src="" width="8" height="8" /></div>
+		<script type="text/javascript">statusIcon('.$row['status'].', function(image){ document.getElementById("status_icon'.$counter.'").src = image; });</script>
+	</div>
+	';
+    $counter++;
+}
+
+?>
+
+<!--<script type="text/javascript">statusIcon('.$row['status'].', function(image){ return image;});</script>-->
